b182ca285409d44e911f06fa61aa2844a456dea9
Filip <siposlaw95@gmail.com>
2022-10-07 12:32:34 +0200

    Commit

--- src/Users.js
+++ src/Users.js
@@ -1,18 +1,44 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./Users.css";
 import UsersList from "./UsersList";
 
 class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: [] };
+    this.input = React.createRef();
+  }
+  addUser = (event) => {
+    event.preventDefault();
+    let newuser = this.input.current.value;
+    this.setState((state) => {
+      return { users: state.users.concat(newuser) };
+    });
+    this.input.current.value = "";
+  };
+  removeUser = (userIndex) => {
+    this.setState((state) => {
+      return {
+        users: state.users.filter((user, index) => {
+          
+          index !== userIndex;
+        }),
+      };
+    });
+  };
   render() {
     return (
       <div className="user-main">
         <h1>User's List</h1>
-        <form>
-          <input type= "text" placeholder="Enter name"/>
-          <button>Add User</button>
+        <form onSubmit={this.addUser}>
+          <input type="text" placeholder="Enter name" ref={this.input} />
+          <button type="submit">Add User</button>
         </form>
-        <UsersList />
+        <UsersList
+          usersList={this.state.users}
+          removeMethod={this.removeUser}
+        />
       </div>
     );
   }
--- src/UsersList.js
+++ src/UsersList.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./UsersList.css";
 
 function UsersList(props) {
+  let usersList = props.usersList;
+  let usersElement = usersList.map((user,index) => {
+    return (
+      <li key={index}>
+        {user} <span onClick={()=>props.removeMethod(index)}>X</span>
+      </li>
+    );
+  });
   return (
     <ul className="list">
-      <li>
-        Filip <span>X</span>
-      </li>
-      <li>
-        Julia <span>X</span>
-      </li>
+      {usersElement}
     </ul>
   );
 }
